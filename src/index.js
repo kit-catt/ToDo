e26c68b8c7be6125e66005c127a8d8963b88eeba
kit-catt <92988903+kit-catt@users.noreply.github.com>
2021-11-16 00:07:55 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+
+const taskValue = document.getElementsByClassName("task_value")[0];
+const taskSubmit = document.getElementsByClassName("task_submit")[0];
+const taskList = document.getElementsByClassName("task_list")[0];
+
+//追加ボタンを作成
+const addTasks = (task) => {
+  // 入力したタスクを追加・表示
+  const listItem = document.createElement("li");
+  const showItem = taskList.appendChild(listItem);
+  showItem.innerHTML = task;
+
+  //タスクに完了ボタンを付与
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = "完了";
+  listItem.appendChild(completeButton);
+
+  //完了ボタンをクリックし、イベントを発動
+  completeButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    completeTask(completeButton);
+  });
+
+  //タスクに削除ボタンを付与
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "削除";
+  listItem.appendChild(deleteButton);
+
+  //削除ボタンをクリックし、イベントを発動（タスクが削除）
+  deleteButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    deleteTasks(deleteButton);
+  });
+};
+
+//完了ボタンの<Li>にクラス名をつける
+const completeTask = (completeButton) => {
+  const targetTask = completeButton.closest("li");
+  targetTask.classList.add("isComplete");
+  targetTask.removeChild(completeButton);
+};
+
+//削除ボタンにタスクを消す機能を付与
+const deleteTasks = (deleteButton) => {
+  const chosenTask = deleteButton.closest("li");
+  taskList.removeChild(chosenTask);
+};
+
+//追加ボタンをクリックし、イベントを発動（タスクが追加）
+taskSubmit.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const task = taskValue.value;
+  addTasks(task);
+  taskValue.value = "";
+});
